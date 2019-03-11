447926127a7c773c0af035de07601a7aa619fd32
Vchalem <Vchalem@gmail.com>
2019-03-11 14:19:55 -0400

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -6,7 +6,7 @@ sfdx force:org:create -f config/project-scratch-def.json -a FSCADK2 --setdefault
 sfdx force:source:push 
 
 sfdx force:user:permset:assign -n Lightning_Components_Sample_Pack_Recruiting
-
+sfdx force:data:tree:import --plan sfdx-out/plan.json 
 
 
 sfdx force:org:open
